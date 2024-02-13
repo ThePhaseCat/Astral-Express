476cfa5b0bd75a582042edd247a3812e3f06b96c
ThePhaseCat <alexmgamer10@gmail.com>
2024-02-13 15:40:53 -0700

    renamed stuff because yes

---
+++ src/client/java/phase/astral_express/AstralExpressClient.java
@@ -0,0 +1,12 @@
+package phase.astral_express;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class AstralExpressClient implements ClientModInitializer
+{
+
+    @Override
+    public void onInitializeClient() {
+        System.out.println("Astral Express Client is running!");
+    }
+}
---
+++ src/main/java/phase/astral_express/AstralExpress.java
@@ -0,0 +1,11 @@
+package phase.astral_express;
+
+import net.fabricmc.api.ModInitializer;
+
+public class AstralExpress implements ModInitializer
+{
+    @Override
+    public void onInitialize() {
+        // TODO Auto-generated method stub
+    }
+}
